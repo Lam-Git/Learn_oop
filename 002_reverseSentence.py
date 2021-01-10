bbe25c12ca65ad366f0dc8833c3d5974e87995dc
Lam Nguyen <lsnguyen0@gmail.com>
2021-01-09 22:43:53 -0800

    commit

---
+++ 002_reverseSentence.py
@@ -0,0 +1,15 @@
+class ReverseSentence:
+    def __init__(self, sentence="Hello World, everything will be just fine."):
+        self.sentence = sentence
+
+    def reverse(self):
+        return " ".join(self.sentence.split(" ")[::-1])
+
+
+if __name__ == "__main__":
+    sentence = input("Please write your sentence: ")
+    reverse_sentence = ReverseSentence(sentence)  # setting up the variable
+    print(
+        "The reversed sentence is: ", reverse_sentence.reverse()
+    )  # this code take the instance method and the second instance
+
